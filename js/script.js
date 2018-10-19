8eef4d4180fffe45489d034c27ef6196100d5523
kjulia <julia.kirilenko96@gmail.com>
2018-10-20 01:03:59 +0300

    form validation

---
+++ js/script.js
@@ -0,0 +1,70 @@
+$(function() {
+
+	$('#signUp').validate({
+
+		rules: {
+			name: {
+				required: true,
+				minlength: 5,
+				maxlength: 15,
+			},
+
+			secondname: {
+				required: true,
+				minlength: 5,
+				maxlength: 15,
+			},
+
+			email: {
+				required: true,
+				email: true,
+			},
+
+			gender: {
+				required: true,
+			},
+
+			pass: {
+				required: true,
+				minlength: 8,
+				maxlength: 16,
+			},
+
+			checkbox: {
+				required: true,
+			},
+		}
+	});
+
+	$('#signUp').on('submit', function (e) {
+		e.preventDefault();
+		if (!$('#signUp').valid()) {
+			return false;
+		}
+		var formData = getFormData();
+		$.ajax({
+			type: 'POST',
+			url: 'http://codeit.pro/codeitCandidates/serverFrontendTest/user/registration',
+			data: formData,
+		})
+		.done(function(response) {
+			alert( response );
+		})
+		.fail(function(error) {
+			console.log(error);
+			alert( error.message );
+		})
+	});
+
+
+	function getFormData() {
+		var arr = $('#signUp').serializeArray()
+		var result = {};
+		for (var i = 0; i < arr.length; i++) {
+			result[arr[i].name] = arr[i].value;
+		}
+
+		return result;
+	}
+
+});
