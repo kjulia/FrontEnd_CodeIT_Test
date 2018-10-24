d42e2ed18fee969ee82cbe978e907513955ede8b
kjulia <julia.kirilenko96@gmail.com>
2018-10-25 00:27:43 +0300

    editing

--- js/script.js
+++ js/script.js
@@ -1,81 +1,81 @@
 $(function() {
 
-	var signup = $('#signUp');
+  var signup = $('#signUp');
 
-	signup.validate({
-		rules: {
-			name: {
-				required: true,
-				minlength: 5,
-				maxlength: 15,
-			},
+  signup.validate({
+    rules: {
+      name: {
+        required: true,
+        minlength: 5,
+        maxlength: 15,
+      },
 
-			secondname: {
-				required: true,
-				minlength: 5,
-				maxlength: 15,
-			},
+      secondname: {
+        required: true,
+        minlength: 5,
+        maxlength: 15,
+      },
 
-			email: {
-				required: true,
-				email: true,
-			},
+      email: {
+        required: true,
+        email: true,
+      },
 
-			gender: {
-				required: true,
-			},
+      gender: {
+        required: true,
+      },
 
-			pass: {
-				required: true,
-				minlength: 8,
-				maxlength: 16,
-			},
+      pass: {
+        required: true,
+        minlength: 8,
+        maxlength: 16,
+      },
 
-			checkbox: {
-				required: true,
-			},
-		}
-	});
+      checkbox: {
+        required: true,
+      },
+    }
+  });
 
-	signup.on('submit', function (e) {
-		e.preventDefault();
-		if (!signup.valid()) {
-			return false;
-		}
-		var formData = getFormData();
-		$.ajax({
-			type: 'POST',
-			url: 'http://codeit.pro/codeitCandidates/serverFrontendTest/user/registration',
-			data: formData,
-		})
-		.done(function(response) {
-			if (response.status === "OK") {
-				setTimeout(function() {
-					window.location.href = './pages/companies.html';
-				}, 100);
-			} else if (response.status === "Form Error") {
-				var validator = signup.validate();
-				var error_object = {};
-				error_object[response.field] = response.message;
-				validator.showErrors(error_object);
-			} else {
-				alert(response.message);
-			}
-		})
-		.fail(function(error) {
-			console.log(error);
-		})
-	});
+  signup.on('submit', function (e) {
+    e.preventDefault();
+    if (!signup.valid()) {
+      return false;
+    }
+    var formData = getFormData();
+    $.ajax({
+      type: 'POST',
+      url: 'http://codeit.pro/codeitCandidates/serverFrontendTest/user/registration',
+      data: formData,
+    })
+    .done(function(response) {
+      if (response.status === "OK") {
+        setTimeout(function() {
+          window.location.href = './pages/companies.html';
+        }, 100);
+      } else if (response.status === "Form Error") {
+        var validator = signup.validate();
+        var error_object = {};
+        error_object[response.field] = response.message;
+        validator.showErrors(error_object);
+      } else {
+        alert(response.message);
+      }
+    })
+    .fail(function(error) {
+      console.log(error);
+    })
+  });
 
 
-	function getFormData() {
-		var arr = $('#signUp').serializeArray()
-		var result = {};
-		for (var i = 0; i < arr.length; i++) {
-			result[arr[i].name] = arr[i].value;
-		}
+  function getFormData() {
+    var arr = $('#signUp').serializeArray()
+    var result = {};
+    for (var i = 0; i < arr.length; i++) {
+      result[arr[i].name] = arr[i].value;
+    }
 
-		return result;
-	}
+    return result;
+  }
 
 });
--- js/script1.js
+++ js/script1.js
@@ -1,8 +1,8 @@
 $.ajax('http://codeit.pro/codeitCandidates/serverFrontendTest/company/getList')
 .done(function(data){
-	$.each(data.list,function(key,item){
-		console.log(item);
-		$('#companyList').append(`<dt>${item.name}</dt>`);
-	})
+  $.each(data.list,function(key,item){
+    console.log(item);
+    $('#companyList').append(`<dt>${item.name}</dt>`);
+  })
 });
 
